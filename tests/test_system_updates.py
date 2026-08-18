f38af02d0b27a920798509fd5391352f95c96c23
espetocearensesobral-boop <2.9255143e+08+espetocearensesobral-boop@users.noreply.github.com>
2026-08-18 14:37:25 +0000

    style: unify process filters and table overflow

--- tests/test_system_updates.py
+++ tests/test_system_updates.py
@@ -6,8 +6,8 @@ from data import system_updates
 from data.update_config import load_update_settings
 
 
-CURRENT_VERSION = "3.28.10"
-NEXT_VERSION = "3.28.11"
+CURRENT_VERSION = "3.28.11"
+NEXT_VERSION = "3.28.12"
 
 
 def _login_admin(client):
