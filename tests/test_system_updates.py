a9e9f638dc10eeadf4fd26babacb1c00b7dece11
espetocearensesobral-boop <2.9255143e+08+espetocearensesobral-boop@users.noreply.github.com>
2026-08-19 11:53:22 +0000

    refactor: rebuild settings markup from scratch

--- tests/test_system_updates.py
+++ tests/test_system_updates.py
@@ -6,8 +6,8 @@ from data import system_updates
 from data.update_config import load_update_settings
 
 
-CURRENT_VERSION = "3.28.40"
-NEXT_VERSION = "3.28.41"
+CURRENT_VERSION = "3.28.41"
+NEXT_VERSION = "3.28.42"
 
 
 def _login_admin(client):
